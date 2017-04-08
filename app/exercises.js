1a35c627fc123c2f3a14fc1171bf0ab84d1e427a
memuna <mmharuna16@gmail.com>
2017-04-08 09:06:14 +0100

    created just enough code to pass my aritGeo algorithm test

--- app/exercises.js
+++ app/exercises.js
@@ -17,6 +17,37 @@ module.exports = {
         return num;
     }
 
+},
+
+aritGeo: function (arr){
+    var result = "";
+    if(arr.length == 0){
+        return 0;
+    }
+
+    if(arr.length <= 2){
+        return arr;
+    }
+
+    else{
+        for(var i = 2; i < arr.length - 1; i++){
+            let a = arr[1] - arr[0];
+            let r = arr[1]/arr[0];
+            if((arr[i + 1] - arr[i]) === a){
+                result = "Arithmetic"; 
+            }
+            else if((arr[i] / arr[i -1]) === r){
+                result = "Geometric";
+            }
+
+            else{
+                result = "-1"
+                result = parseInt(result);
+            }
+         }
+    }
+
+    return result;
 }
 
 }
