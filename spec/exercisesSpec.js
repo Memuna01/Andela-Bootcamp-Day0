c42dc2263f543125b98f134edcbbea500108479c
memuna <mmharuna16@gmail.com>
2017-04-08 08:08:31 +0100

    wrote just enough code to pass the fizzBuzz question

--- spec/exercisesSpec.js
+++ spec/exercisesSpec.js
@@ -1,38 +1,38 @@
-var myApp = require('../app/exercises.js');
+var app = require('../app/exercises.js');
 
 
 describe("Fizz Buzz tests ", function() {
 
   it("should return `Fizz` for number divisible by 3", function() {
-    expect(myApp.fizzBuzz(3)).toBe('Fizz');
+    expect(app.fizzBuzz(3)).toBe('Fizz');
   });
 
   it("should return `Buzz` for number divisible by 5", function() {
-    expect(myApp.fizzBuzz(5)).toBe('Buzz');
+    expect(app.fizzBuzz(5)).toBe('Buzz');
   });
 
   it("should return `FizzBuzz` for 15", function() {
-    expect(myApp.fizzBuzz(15)).toBe('FizzBuzz');
+    expect(app.fizzBuzz(15)).toBe('FizzBuzz');
   });
 
   it("should return `FizzBuzz` for 45", function() {
-    expect(myApp.fizzBuzz(45)).toBe('FizzBuzz');
+    expect(app.fizzBuzz(45)).toBe('FizzBuzz');
   });
 
   it("should return `FizzBuzz` for 90", function() {
-    expect(myApp.fizzBuzz(90)).toBe('FizzBuzz');
+    expect(app.fizzBuzz(90)).toBe('FizzBuzz');
   });
 
   it("should return `Fizz` for 63", function() {
-    expect(myApp.fizzBuzz(63)).toBe('Fizz');
+    expect(app.fizzBuzz(63)).toBe('Fizz');
   });
 
   it("should return 7 since its indivisible by 3 and 5", function() {
-    expect(myApp.fizzBuzz(7)).toBe(7);
+    expect(app.fizzBuzz(7)).toBe(7);
   });
 
   it("should return 101 since its indivisible by 3 and 5", function() {
-    expect(myApp.fizzBuzz(101)).toBe(101);
+    expect(app.fizzBuzz(101)).toBe(101);
   });
 
 });
@@ -45,7 +45,7 @@ describe("Fizz Buzz tests ", function() {
     describe("Case for an empty array", function() {
 
       it("should return 0 for an empty array", function() {
-        expect(aritGeo([])).toEqual(0);
+        expect(app.aritGeo([])).toEqual(0);
       });
 
     });
@@ -53,15 +53,15 @@ describe("Fizz Buzz tests ", function() {
     describe("Case for an arithmetic sequence", function() {
 
       it("should return `Arithmetic` for [2, 4, 6, 8, 10]", function() {
-        expect(aritGeo([2, 4, 6, 8, 10])).toEqual('Arithmetic');
+        expect(app.aritGeo([2, 4, 6, 8, 10])).toEqual('Arithmetic');
       });
 
       it("should return `Arithmetic` for [5, 11, 17, 23, 29, 35, 41]", function() {
-        expect(aritGeo([5, 11, 17, 23, 29, 35, 41])).toEqual('Arithmetic');
+        expect(app.aritGeo([5, 11, 17, 23, 29, 35, 41])).toEqual('Arithmetic');
       });
 
       it("should return `Arithmetic` for [15, 10, 5, 0, -5, -10]", function() {
-        expect(aritGeo([15, 10, 5, 0, -5, -10])).toEqual('Arithmetic');
+        expect(app.aritGeo([15, 10, 5, 0, -5, -10])).toEqual('Arithmetic');
       });
 
     });
@@ -69,15 +69,15 @@ describe("Fizz Buzz tests ", function() {
     describe("Case for a geometric sequence", function() {
 
       it("should return `Geometric` for [2, 6, 18, 54, 162]", function() {
-        expect(aritGeo([2, 6, 18, 54, 162])).toEqual('Geometric');
+        expect(app.aritGeo([2, 6, 18, 54, 162])).toEqual('Geometric');
       });
 
       it("should return `Geometric` for [0.5, 3.5, 24.5, 171.5]", function() {
-        expect(aritGeo([0.5, 3.5, 24.5, 171.5])).toEqual('Geometric');
+        expect(app.aritGeo([0.5, 3.5, 24.5, 171.5])).toEqual('Geometric');
       });
 
       it("should return `Geometric` for [−128, 64, −32, 16, −8]", function() {
-        expect(aritGeo([-128, 64, -32, 16, -8])).toEqual('Geometric');
+        expect(app.aritGeo([-128, 64, -32, 16, -8])).toEqual('Geometric');
       });
 
     });
@@ -85,15 +85,15 @@ describe("Fizz Buzz tests ", function() {
     describe("Case for neither arithmetic nor geometric sequence", function() {
 
       it("should return -1 for [1, 2, 3, 5, 8]", function() {
-        expect(aritGeo([1, 2, 3, 5, 8])).toEqual(-1);
+        expect(app.aritGeo([1, 2, 3, 5, 8])).toEqual(-1);
       });
 
       it("should return -1 for [1, 3, 6, 10, 15]", function() {
-        expect(aritGeo([1, 3, 6, 10, 15])).toEqual(-1);
+        expect(app.aritGeo([1, 3, 6, 10, 15])).toEqual(-1);
       });
 
       it("should return -1 for [1, 8, 27, 64, 125]", function() {
-        expect(aritGeo([1, 8, 27, 64, 125])).toEqual(-1);
+        expect(app.aritGeo([1, 8, 27, 64, 125])).toEqual(-1);
       });
       
     });
